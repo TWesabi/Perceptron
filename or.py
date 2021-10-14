69e365a7264d4f4627fc87186bd32bb96106ba78
unknown <TWesabi>
2021-10-14 10:58:45 -0700

    final or

--- or.py
+++ or.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import joblib
+import os
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot, save_model
 
 OR = {
     "x1": [0,0,1,1],
@@ -8,4 +12,19 @@ OR = {
 
 df = pd.DataFrame(OR)
 
-df
+print(df)
+
+X,y = prepare_data(df)
+
+ETA = 0.3 # 0 and 1
+EPOCHS = 10
+
+model = Perceptron(eta=ETA, epochs=EPOCHS)
+model.fit(X, y)
+
+_ = model.total_loss()
+
+
+save_model(model, filename = "or.model")
+
+save_plot(df, model ,file_name= "or.png" )
